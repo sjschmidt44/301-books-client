316a0f7874c45f5af7e104eb8cee5419e2ba394f
Scott Schmidt <sjschmidt44@gmail.com>
2017-10-31 10:15:55 -0700

    Update routes.js

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -6,16 +6,4 @@ page('/books/search', app.bookController.search);
 page('/books/:book_id/update', app.bookController.fetch, app.bookController.update);
 page('/books/:book_id', app.bookController.detail, app.adminController.verify);
 page('/admin', app.adminController.sign);
-// page('*', function(ctx) {
-//   page.redirect('home')
-//   let baseRoutes = ['home', 'books', 'admin'];
-//   if(baseRoutes.includes(ctx.params[0])) {
-//     console.log('wat',ctx);
-//     return
-//   } else {
-//     page.redirect('/home')
-//     console.log('why', ctx);
-//   }
-// });
-page({hashbang: true});
-// page();
+page();
