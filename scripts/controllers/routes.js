4be6762754b9e8b01ec534254837897ca5982174
Scott Schmidt <sjschmidt44@gmail.com>
2017-10-31 10:12:54 -0700

    Update routes.js

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,11 +1,11 @@
 'use strict'
 
-page('home', app.bookController.index, app.adminController.verify);
-page('books/new', app.bookController.add);
-page('books/search', app.bookController.search);
-page('books/:book_id/update', app.bookController.fetch, app.bookController.update);
-page('books/:book_id', app.bookController.detail, app.adminController.verify);
-page('admin', app.adminController.sign);
+page('/home', app.bookController.index, app.adminController.verify);
+page('/books/new', app.bookController.add);
+page('/books/search', app.bookController.search);
+page('/books/:book_id/update', app.bookController.fetch, app.bookController.update);
+page('/books/:book_id', app.bookController.detail, app.adminController.verify);
+page('/admin', app.adminController.sign);
 // page('*', function(ctx) {
 //   page.redirect('home')
 //   let baseRoutes = ['home', 'books', 'admin'];
